afdf851d0eeb3cbb8733425f90b585f1406f9b7a
adesdesk <aadelakun28@gmail.com>
2024-06-10 16:47:00 +0100

    add nodemon

--- server/src/index.js
+++ server/src/index.js
@@ -18,3 +18,51 @@ let answers = [
   ];
 
 // define routes needed on the front end
+app.get('/api/answers', (req, res) => {
+    try {
+      res.status(200).json(answers);
+    } catch (error) {
+      console.error('Failed to get answers', error);
+    }
+  });
+  
+  app.post('/api/answers', (req, res) => {
+    try {
+      const newTodo = { id: answers.length + 1, task: req.body.task };
+      answers.push(newTodo);
+  
+      res.status(201).json(newTodo);
+    } catch (error) {
+      console.error('Failed to submit answer', error);
+    }
+  });
+  
+  app.put('/api/answers/:id', (req, res) => {
+    try {
+      const id = parseInt(req.params.id);
+      const answer = answers.find(t => t.id === id);
+  
+      if (!answer) {
+        res.status(404).send('Todo not found');
+  
+        return;
+      }
+  
+      answer.task = req.body.task;
+  
+      res.status(200).json(answer);
+    } catch (error) {
+      console.error('failed to edit answer', error);
+    }
+  });
+  
+  app.delete('/api/answers/:id', (req, res) => {
+    try {
+      const id = parseInt(req.params.id);
+      answers = answers.filter(t => t.id !== id);
+  
+      res.status(204).send();
+    } catch (error) {
+      console.error('failed to delete answer', error);
+    }
+  });
